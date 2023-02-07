26f2e11516c7eb5a4e2c8797beb72767c755475b
Lilibeth Sequeira <Lilibeth.Sequeira@ooqia.com>
2023-02-07 14:41:58 -0600

    feat(note): create controller, factory, seeder and migation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Note;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'email'    => 'l@admin.com',
+            'password' => bcrypt('123456')
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Note::factory(30)->create();
     }
 }
